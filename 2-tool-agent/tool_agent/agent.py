813ad9a65498a83acf4771b0ddde9742ea013d83
tlukanie <tanyanewem@gmail.com>
2026-01-22 14:06:36 +0100

    added datetime library for the example #2

--- 2-tool-agent/tool_agent/agent.py
+++ 2-tool-agent/tool_agent/agent.py
@@ -1,13 +1,14 @@
+from datetime import datetime
 from google.adk.agents import Agent
 from google.adk.tools import google_search
 
-# def get_current_time() -> dict:
-#     """
-#     Get the current time in the format YYYY-MM-DD HH:MM:SS
-#     """
-#     return {
-#         "current_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-#     }
+def get_current_time() -> dict:
+    """
+    Get the current time in the format YYYY-MM-DD HH:MM:SS
+    """
+    return {
+        "current_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    }
 
 root_agent = Agent(
     name="tool_agent",
@@ -15,9 +16,9 @@ root_agent = Agent(
     description="Tool agent",
     instruction="""
     You are a helpful assistant that can use the following tools:
-    - google_search
+    - get_current_time
     """,
-    tools=[google_search],
-    # tools=[get_current_time],
+    # tools=[google_search],
+    tools=[get_current_time],
     # tools=[google_search, get_current_time], # <--- Doesn't work
 )
